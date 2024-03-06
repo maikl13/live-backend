1ca7c2010bdfa6497d2be73c815b37a59ea5a022
root <root@mail.tik-show.com>
2024-03-06 12:06:49 +0000

    eslam

--- add_view_to_room.php
+++ add_view_to_room.php
@@ -8,8 +8,7 @@ include "config.php";
 $user_uid = $_GET['user_uid'];
  $viewed = $_GET['viewed'];
  
-
-      $result = updateSql("INSERT INTO `viewed_rooms_search_results` (`id`, `searcher_uid`, `room_id`) VALUES (NULL, '$user_uid', '$viewed');");
+ $result = updateSql("INSERT INTO `viewed_rooms_search_results` (`id`, `searcher_uid`, `room_id`) VALUES (NULL, '$user_uid', '$viewed');");
 
 
 echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
